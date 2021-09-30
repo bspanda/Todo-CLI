1a4231b56c188bca642b1b77d81d16afa02a11d5
BSPANDA <panda.bs@raghuenggcollege.in>
2021-09-30 09:57:16 +0530

    Todo CLI Project

---
+++ todo.rb
@@ -0,0 +1,63 @@
+require "active_record"
+
+class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def overdue?
+    due_date < Date.today
+  end
+
+  def due_later?
+    due_date > Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id} #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.due_today
+    where(due_date: Date.today)
+  end
+
+  def self.overdue
+    where(due_date: ...Date.today)
+  end
+
+  def self.due_later
+    where(due_date: Date.today...)
+  end
+
+  #def self.to_displayable_string
+  # all.map { |todo| todo.to_displayable_list }
+  #end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+    puts "Overdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.add_task(h)
+    create(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete(todo_id)
+    mark_complete = Todo.find(todo_id)
+    mark_complete.completed = true
+    mark_complete.save
+    mark_complete
+  end
+end
